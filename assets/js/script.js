76e0f2201fff3e835e752f117c409ccf2e9f2265
KingAusti <austin@thehenrys.xyz>
2022-03-30 17:59:08 -0700

    finalizing mvp code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,11 @@ let currentTime;
 
 //pull date and time
 function updateDateTime() {    
-    currentDate = moment(new Date());
-    currentTime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+    currentDate = moment().format('dddd, MMMM Do, YYYY');
+    currentTime = moment().format('h:mm:ss a');
+    //adding time and date to html
+    $('#currentDay').text(currentDate);
+    $('#currentTime').text(currentTime);
 };
 
 
@@ -15,7 +18,7 @@ $(document).ready(function(){
     //setting update to every second(1000ms)
     setInterval(updateDateTime, 1000);
     let rows = $('.row');
-    let currentHour = parseInt(currentDate.format('k'));
+    let currentHour = parseInt(moment().format('k'));
     //for loop to check time and compare it to the block where text is saved
     for (let i = 0; i < rows.length; i++){
         if(parseInt(rows.eq(i).attr('id')) < currentHour){
@@ -28,14 +31,14 @@ $(document).ready(function(){
     };
     
     //loading saved data for each hour from localStorage    
-    $("#content-9").attr("value", localStorage.getItem('content-9'));
-    $("#content-10").attr("value", localStorage.getItem('content-10'));
-    $("#content-11").attr("value", localStorage.getItem('content-11'));
-    $("#content-12").attr("value", localStorage.getItem('content-12'));
-    $("#content-13").attr("value", localStorage.getItem('content-13'));
-    $("#content-14").attr("value", localStorage.getItem('content-14'));
-    $("#content-15").attr("value", localStorage.getItem('content-15'));
-    $("#content-16").attr("value", localStorage.getItem('content-16'));
+    $("#content-9").val(localStorage.getItem('content-9'));
+    $("#content-10").val(localStorage.getItem('content-10'));
+    $("#content-11").val(localStorage.getItem('content-11'));
+    $("#content-12").val(localStorage.getItem('content-12'));
+    $("#content-13").val(localStorage.getItem('content-13'));
+    $("#content-14").val(localStorage.getItem('content-14'));
+    $("#content-15").val(localStorage.getItem('content-15'));
+    $("#content-16").val(localStorage.getItem('content-16'));
 
     //eventlistener for click event on each save button
     $(".fas").on("click", function(event) {
